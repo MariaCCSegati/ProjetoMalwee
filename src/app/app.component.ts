b8f6ceaec1efe18bbb14a734b5b6375fdaaea789
Maria Cecília Coelho Segati <109155054+MariaCCSegati@users.noreply.github.com>
2022-09-15 16:39:14 -0300

    login2 - 15/09

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cadastro';
+
+  constructor(private http : HttpClient) {}
+
+  rootURL = '/api';
+  
+  
+
+  
+}
+
+
